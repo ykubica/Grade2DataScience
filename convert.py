410c2252f097da04e7c43abe3d41025143320b49
ykubica <ykubica@hotmail.co.uk>
2024-05-28 20:41:07 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
-# Load the Excel file
 file_path = 'Grade2Results.xlsx'
 df = pd.read_excel(file_path, sheet_name='Sheet1')
 
-# Save the DataFrame to a CSV file
 csv_file_path = 'Grade2Results.csv'
 df.to_csv(csv_file_path, index=False)
